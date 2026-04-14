9f48e92a9fc8b8aaf0b4b4f470b3aac3e93efc9c
smbs-techno <srimashissmbs2005@gmail.com>
2026-04-14 06:58:13 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score=true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let value=Number(score)
+console.log(typeof (value));
+console.log(value);
+
+// "30"->30
+// "30abc"->NaN(Not a Number)
+// true->1; false->0
+
+let isLoggedIn="Sri"
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1->true; 0->false
+// ""->false; "Sri"->true
+
+let sum=40
+let St=String(sum)
+console.log(St);
+console.log(typeof St);
+
+
+
