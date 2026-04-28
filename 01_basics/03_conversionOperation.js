1837a6c308287f23aae46c1126172bb9bf59c6b6
“smbs-techno” <srimashissmbs2005@gmail.com>
2026-04-28 18:54:42 +0530

    conversion and operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score=true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let value=Number(score)
-console.log(typeof (value));
-console.log(value);
+// console.log(typeof (value));
+// console.log(value);
 
 // "30"->30
 // "30abc"->NaN(Not a Number)
@@ -13,15 +13,53 @@ console.log(value);
 
 let isLoggedIn="Sri"
 let booleanisLoggedIn=Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
 // 1->true; 0->false
 // ""->false; "Sri"->true
 
 let sum=40
 let St=String(sum)
-console.log(St);
-console.log(typeof St);
+// console.log(St);
+// console.log(typeof St);
+
+// ------operations------
+let v=3;
+let neg=-v;
+// console.log(neg);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello";
+let str2=" Sri";
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(+true); o/p : 1
+// console.log(true+); o/p : error
+// console.log(+""); o/p : 0
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let game=100
+game++
+console.log(game);
+
+let game1=100
+++game1
+console.log(game1);
+
 
 
 
