c66a8f89a14491896ab6e013857bdf27201a9980
“smbs-techno” <srimashissmbs2005@gmail.com>
2026-04-29 18:56:28 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1); //o/p-true
+// console.log("02" > 1); //o/p-true
+
+// console.log(null>0); //o/p-false
+// console.log(null==0); //o/p-false
+// console.log(null>=0); //o/p-true
+
+// console.log(undefined==0); //o/p-false
+// console.log(undefined>0); //o/p-false
+// console.log(undefined<0); //o/p-false
+
+// ===(strict check)
+// console.log("2" === 2); //false - checks data type
