46c578364f8096d36b150acd165beb4db23f1c61
“smbs-techno” <srimashissmbs2005@gmail.com>
2026-05-01 19:04:05 +0530

    Number and Maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+const score=100
+// console.log(score);
+
+const balance=new Number(200)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const other=39.4667
+// console.log(other.toPrecision(3));
+// console.log(other.toPrecision(2));
+
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ------Maths------
+// console.log(Math);
+// console.log(Math.abs(-5)); //always return positive value
+// console.log(Math.round(5.2));
+// console.log(Math.round(5.7));
+// console.log(Math.ceil(5.3));
+// console.log(Math.floor(5.8));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min +1))+min) 
